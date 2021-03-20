997ce1a9ffa487addc6fb084282bab6144881085
DawsonChang <tiamat2009@hotmail.com>
2021-03-21 00:39:23 +0100

    add element-ui components On demand

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,17 @@
+// https://element.eleme.io/#/zh-CN/component/quickstart
+// 照 element-ui 官網說的按需引入，但看這篇https://www.jianshu.com/p/2181f3c263c2
+// 現在已經沒有.babelrc 變成 babel.config.js
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
+  ],
+  plugins: [
+    [
+      "component",
+      {
+        "libraryName": "element-ui",
+        "styleLibraryName": "theme-chalk"
+      }
+    ]
   ]
 }
